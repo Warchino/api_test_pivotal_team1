36dd444338c899a16c0bdbd3137698c677d7d669
Rodrigo Menacho <rmenacho21@gmail.com>
2019-02-04 23:39:16 -0400

    Acceptance test of project history

--- definitions.py
+++ definitions.py
@@ -7,7 +7,8 @@ ENV_YML = os.path.join(ROOT_DIR, 'environment.yml')
 SCHEMA_PIVOTAL = {
     'Account': os.path.join(ROOT_DIR, 'schemas/pivotal_tracker/account.schema.json'),
     'Project': os.path.join(ROOT_DIR, 'schemas/pivotal_tracker/project.schema.json'),
-    'Projects': os.path.join(ROOT_DIR, 'schemas/pivotal_tracker/projects.schema.json')
+    'Projects': os.path.join(ROOT_DIR, 'schemas/pivotal_tracker/projects.schema.json'),
+    'Project-History': os.path.join(ROOT_DIR, 'schemas/pivotal_tracker/project_history.schema.json')
 }
 SCHEMA_TODOLY = {
     'Creation': os.path.join(ROOT_DIR, 'schemas/todoly/projectcreation.schema.todoly.json')
---
+++ features/project/pivotal_tracker/acceptance/project_history_acceptance.feature
@@ -0,0 +1,9 @@
+@acceptance
+Feature: Project History Acceptance Test
+
+  @create_project
+  Scenario: Get an specified project history
+    Given I set up a "GET" request to "/projects/{project_id}/history/days" endpoint
+    When I send the request
+    Then I get a "200" status code as response
+    And I validate with "Project-History" schema
